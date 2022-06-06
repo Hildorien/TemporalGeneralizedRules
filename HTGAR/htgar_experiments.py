27e92b79e1ff29d5fc229a71892d60775e3176bd
trekkar <noggaxiii@gmail.com>
2022-06-06 18:30:54 -0300

    Some last experiments modifications to be pushed

--- HTGAR/htgar_experiments.py
+++ HTGAR/htgar_experiments.py
@@ -18,9 +18,9 @@ def htgar_experiment(min_sup, min_conf, min_r):
                                              'single',
                                              "F:\TesisSyntheticDatasets\Root\R1000T250k.tax", True)
 
-    # HTAR_BY_PG(database, min_sup, min_conf, min_sup)
+    HTAR_BY_PG(database, min_sup, min_conf, min_sup)
     print('########################################')
-    # HTAR_BY_PG(database_with_taxonomy, min_sup, min_conf, min_sup, True)
+    HTAR_BY_PG(database_with_taxonomy, min_sup, min_conf, min_sup, True)
     print('########################################')
     HTAR_BY_PG(database_with_taxonomy, min_sup, min_conf, min_sup, True, min_r)
 
@@ -31,16 +31,16 @@ def htgar_foodmart_experiment(min_sup, min_conf, min_r):
         3. Run HTGAR (with R-interesting parameter) and print amount of rules
         :return:
         """
-    # foodmart_98 = Parser().parse("Datasets/sales_formatted_1998_sorted_by_timestamp.csv",
-    #                           'single', None, True)
-    foodmart_98_with_taxonomy = Parser().parse("../Datasets/sales_formatted_1998_sorted_by_timestamp.csv",
+    foodmart_98 = Parser().parse("Datasets/sales_formatted_1998_sorted_by_timestamp.csv",
+                              'single', None, True)
+    foodmart_98_with_taxonomy = Parser().parse("Datasets/sales_formatted_1998_sorted_by_timestamp.csv",
                                                'single',
-                                               '../Taxonomies/salesfact_taxonomy_single_2.csv', True)
+                                               'Taxonomies/salesfact_taxonomy_single_2.csv', True)
 
-    # HTAR_BY_PG(foodmart_98, min_sup, min_conf, min_sup)
-    # print('########################################')
-    # HTAR_BY_PG(foodmart_98_with_taxonomy, min_sup, min_conf, min_sup, True)
-    # print('########################################')
+    HTAR_BY_PG(foodmart_98, min_sup, min_conf, min_sup)
+    print('########################################')
+    HTAR_BY_PG(foodmart_98_with_taxonomy, min_sup, min_conf, min_sup, True)
+    print('########################################')
     HTAR_BY_PG(foodmart_98_with_taxonomy, min_sup, min_conf, min_sup, True, min_r)
 
 def htgar_experiment_roots(min_supp):
@@ -78,28 +78,33 @@ def htgar_experiment_roots(min_supp):
     #     print('#############################################################')
 
 def htgar_foodmart_experiment_with_r_interesting():
-    # foodmart_97 = Parser().parse("../Datasets/sales_formatted_1997_sorted_by_timestamp.csv",
-    #                           'single', None, True)
+    foodmart_97 = Parser().parse("../Datasets/sales_formatted_1997_sorted_by_timestamp.csv",
+                              'single', None, True)
     foodmart_97_with_taxonomy = Parser().parse("../Datasets/sales_formatted_1997_sorted_by_timestamp.csv",
                                                'single',
                                                '../Taxonomies/salesfact_taxonomy_single_2.csv', True)
-    min_conf = 0.5
-    minsupp = 0.0002
-    min_r = [0, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1]
-    for r in min_r:
-        HTAR_BY_PG(foodmart_97_with_taxonomy, minsupp, min_conf, minsupp, True, r)
+    min_conf = 0.75
+    minsup = 0.002
 
+    # for min_sup in minsupps:
+    #     HTAR_BY_PG(foodmart_97, min_sup, min_conf, min_sup)
+
+    for r in [0, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1]:
+        HTAR_BY_PG(foodmart_97_with_taxonomy, minsup, min_conf, minsup, True, r)
 
 if __name__ == "__main__":
     # htgar_experiment(0.0025, 0.5, 1.1) # First Run
     # htgar_experiment(0.002, 0.5, 1.1)  # Second Run
 
     #Foodmart experiments
-    htgar_foodmart_experiment(0.01, 0.5, 1.1)
+    # htgar_experiment(0.005, 0.5, 1.1)
 
     #HTGAR Roots experiment
     # htgar_experiment_roots(0.005)
 
     #HTGAR Foodmart experiment r interesting
-    # htgar_foodmart_experiment_with_r_interesting()
+    #htgar_foodmart_experiment_with_r_interesting()
+
+    #htgar_foodmart_experiment()
+
 
